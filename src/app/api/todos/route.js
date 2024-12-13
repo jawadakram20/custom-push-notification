bcc2f252ebd9e42a92bcf3a894e24a295ddc9967
Afaq Ahmad <afaq01ahmad@gmail.com>
2024-12-13 18:58:31 +0500

    testing

--- src/app/api/todos/route.js
+++ src/app/api/todos/route.js
@@ -31,7 +31,7 @@ export async function POST(req) {
     };
 
     todos.push(newTodo);
-    await fs.writeFile(filePath, JSON.stringify(todos, null, 2), "utf8");
+    // await fs.writeFile(filePath, JSON.stringify(todos, null, 2), "utf8");
 
     return new Response(JSON.stringify(newTodo), {
       headers: { "Content-Type": "application/json" },
